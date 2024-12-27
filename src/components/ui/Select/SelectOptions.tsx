db604bbdf1f17ef497ea54154d1972d65b4d377f
wahyunup <wahyunurpambuko@gmail.com>
2024-12-27 17:11:08 +0700

    merge: fiture select

--- src/components/ui/Select/SelectOptions.tsx
+++
@@ -1,7 +0,0 @@
-import { SelectProps } from "@/components/@types/ui";
-const SelectOption = ({ children, value }: SelectProps) => {
-    return <option value={value}>{children}</option>;
-  };
-  
-
-export default SelectOption;
