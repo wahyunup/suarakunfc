aad485598a05babbca48a41d31e6b745e34fe85a
aaldiiieee <prmdy02@gmail.com>
2025-01-07 22:12:55 +0700

    fix: adjust dashboard

--- src/app/(dashboard)/dashboard/page.tsx
+++ src/app/(dashboard)/dashboard/page.tsx
@@ -1,21 +1,20 @@
 "use client";
 
 import UserCard from "@/components/ui/Card/UserCard";
-import { useState } from "react";
 
 const DashboardPage = () => {
-  const [dataUser, setDataUser] = useState([
+  const dataUser = [
     {
       username: "AscenditCreative",
       nik: "0987654321",
-      noTelp: +621231123,
+      noTelp: "+621231123",
       userImage:
         "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSoq0f1tSU2b8opZaApGh5tl2FreFb52dyo6Q&s",
     },
-  ]);
+  ];
 
   return (
-    <div>
+    <>
       {dataUser.map((data, index) => (
         <UserCard
           key={index}
@@ -26,7 +25,7 @@ const DashboardPage = () => {
         />
       ))}
       <h1>Dashboard</h1>
-    </div>
+    </>
   );
 };
 
