5f34a42da1966943411fc1fdef1acccf5d70b082
aaldiiieee <prmdy02@gmail.com>
2025-01-08 18:42:01 +0700

    adjust dataUser

--- src/app/(dashboard)/dashboard/page.tsx
+++ src/app/(dashboard)/dashboard/page.tsx
@@ -1,18 +1,17 @@
 "use client";
 
 import UserCard from "@/components/ui/Card/UserCard";
-import { useState } from "react";
 
 const DashboardPage = () => {
-  const [dataUser, setDataUser] = useState([
+  const dataUser = [
     {
       username: "AscenditCreative",
       nik: "0987654321",
       noTelp: +621231123,
       userImage:
         "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSoq0f1tSU2b8opZaApGh5tl2FreFb52dyo6Q&s",
-    },
-  ]);
+    }
+  ]
 
   return (
     <div>
